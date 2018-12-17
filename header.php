ea838e013ed62f864b475b567f415bb047a6aadc
Ludo Mydla <lmmydla@gmail.com>
2018-12-17 23:32:15 +0100

    Post loop added

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700&amp;subset=latin-ext">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,700&amp;subset=latin-ext" rel="stylesheet">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
   <title><?php echo $name.' - '.$desc; ?></title>
 </head>
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-get_header();
-get_footer();
+<?php get_header(); ?>
 
+<div class="content">
+
+<?php 
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+  get_template_part( 'content', get_post_format() );
+endwhile; endif; ?>
+</div>
+
+<?php
+  get_sidebar();
+  get_footer();
 ?>
